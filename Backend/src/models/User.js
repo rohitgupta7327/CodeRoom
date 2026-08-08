84aaf6f83b9e9b0f3596140cb1bdc0841e706d80
rohitgupta7327 <rg3050345@gmail.com>
2026-08-08 09:39:52 +0530

    fixes inngest.js so that it sync with inngest

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -1,29 +1,29 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  name:{
-    type:String,
-    required:true,
+  name: {
+    type: String,
+    required: true,
   },
-  email:{
-    type:String,
-    required:true,
-    unique:true
+  email: {
+    type: String,
+    required: true,
+    unique: true
   },
-  profileImage:{
-    type:String,
-    default:"",
+  profileImage: {
+    type: String,
+    default: "",
   },
-  clearkId:{
-    type:String,
-    required:true,
-    unique:true
+  clearkId: {
+    type: String,
+    required: true,
+    unique: true
   },
 },
-{timestamps:true} // createdAt , updatedAt
+  { timestamps: true } // createdAt , updatedAt
 );
 
 // member size
-const User= mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
--- api/inngest.js
+++ api/inngest.js
@@ -1,4 +1,4 @@
-import { serve } from "inngest/express";
+import { serve } from "inngest/node";
 import { inngest, functions } from "../Backend/src/lib/inngest.js";
 
 export default serve({
