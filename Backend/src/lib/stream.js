0855c0be5d3ecaae7c026f3d3afa5cead8d147d1
rohitgupta7327 <rg3050345@gmail.com>
2026-08-10 00:59:23 +0530

    session-routes& controlles added

--- Backend/src/lib/stream.js
+++ Backend/src/lib/stream.js
@@ -1,5 +1,6 @@
 import { StreamChat } from "stream-chat"
 import { ENV } from "./env.js"
+import { StreamClient } from "@stream-io/node-sdk";
 
 const apiKey = ENV.STREAM_API_KEY
 const apiSecret = ENV.STREAM_API_SECRET
@@ -8,7 +9,9 @@ if (!apiKey || !apiSecret) {
     console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
 }
 
-export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // this is for chat features
+export const streamClient = new StreamClient(apiKey, apiSecret); // this will be used for video calls
+
 
 // create and update data 
 export const upsertStreamUser = async (userData) => {
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -18,6 +18,7 @@ const __dirname = path.resolve();
 
 // middleware
 app.use(express.json());
+
 // credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
@@ -25,6 +26,7 @@ app.use(clerkMiddleware()); // this adds auth field to request object: req.auth(
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
+
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
